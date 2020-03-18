04c3a79dade35f384e6d2548e59b87e2b1b2ad2c
Tom Alexander <tom.h.alexander@googlemail.com>
2020-03-18 13:21:23 +0400

    Immutables for config

--- app/templates/projects/server/java/Configuration.java
+++ app/templates/projects/server/java/Configuration.java
@@ -4,7 +4,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import <%= package %>.backend.DatabaseBackend;
 import <%= package %>.backend.DatabaseConfiguration;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
@@ -12,20 +11,14 @@ import io.dropwizard.setup.Environment;
 /**
  * Server configuration for the <%= name %> API server.
  */
-public final class <%= className %>Configuration extends Configuration {
-    private final DatabaseConfiguration databaseConfiguration;
-
-    @JsonCreator
-    public <%= className %>Configuration(
-            @JsonProperty("database") final DatabaseConfiguration database) {
-        checkNotNull(database);
-
-        this.databaseConfiguration = database;
-    }
-
-    public DatabaseConfiguration getDatabaseConfiguration() { return databaseConfiguration; }
+@Value.Immutable
+@JsonDeserialize(as = Immutable<%= className %>Configuration.class)
+public abstract class <%= className %>Configuration extends Configuration {
+    
+    @JsonProperty("database")
+    abstract DatabaseConfiguration getDatabaseConfiguration();
 
     public DatabaseBackend getDatabaseBackend(Environment environment) {
-        return databaseConfiguration.createDBI(environment, "jdbi-backend").onDemand(DatabaseBackend.class);
+        return getDatabaseConfiguration().createDBI(environment, "jdbi-backend").onDemand(DatabaseBackend.class);
     }
 }
